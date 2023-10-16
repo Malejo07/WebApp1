e1316f57c822858461fbf377d0c785070dcc5ed5
Mario Alejandro Jaramillo <malejo9002@gmail.com>
2023-10-15 21:29:45 -0500

    First commit

---
+++ TODOApp/TODO.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TODO.WebApi.Controllers
+{
+    [Route("api/[controller]")]//Atributo
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+       //Constructor
+        public UsersController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> GetHelloWordl()
+        {
+            return Ok("Hello World");
+        }
+
+    }
+}
